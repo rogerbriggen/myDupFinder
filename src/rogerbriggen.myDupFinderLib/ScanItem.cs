c1f26d3fc7e6af8c05865c3218640101fdcef3a0
Roger Briggen <roger.briggen@bluewin.ch>
2020-09-06 18:54:38 +0200

    add source link (https://github.com/dotnet/sourcelink/blob/master/README.md) add code coverage (https://github.com/coverlet-coverage/coverlet)

---
+++ src/rogerbriggen.myDupFinderLib/ScanItem.cs
@@ -0,0 +1,15 @@
+﻿namespace rogerbriggen.myDupFinderLib
+{
+    public class ScanItem
+    {
+        public bool MyTest()
+        {
+            return true;
+        }
+
+        public bool MyTest2()
+        {
+            return true;
+        }
+    }
+}
---
+++ tests/rogerbriggen.myDupFinderLibUnitTest/ScanItemTest.cs
@@ -0,0 +1,15 @@
+﻿using rogerbriggen.myDupFinderLib;
+using Xunit;
+
+namespace rogerbriggen.myDupFinderLibUnitTest
+{
+    public class ScanItemTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            ScanItem scanItem = new ScanItem();
+            Assert.True(scanItem.MyTest());
+        }
+    }
+}
